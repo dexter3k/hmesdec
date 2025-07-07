f0d2cebc97071a4c0ebc433e41873d9ecce04e9c
dexter3k <schooldev3000@gmail.com>
2025-07-07 08:15:37 +0300

    Some more opcodes

--- cmd/pg/bundle/main.go
+++ cmd/pg/bundle/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/binary"
 	"unicode/utf16"
+	"math"
 	"fmt"
 	"os"
 	"io"
@@ -238,12 +239,10 @@ func parseBundle(r io.ReadSeeker) *Bundle {
 		panic("not implemented")
 	}
 
-	// regexp table
-	for i := uint32(0); i < header.RegExpCount; i++ {
-		var offset, length uint32
-		check(binary.Read(r, binary.LittleEndian, &offset))
-		check(binary.Read(r, binary.LittleEndian, &length))
-	}
+	// regexp table, skip for now
+	regexpOffset, _ := r.Seek(0, io.SeekCurrent)
+	_, err = r.Seek(int64(header.RegExpCount) * 4 * 2, io.SeekCurrent)
+	check(err)
 
 	// regexp storage
 	regexpStorage := make([]byte, header.RegExpStorageSize)
@@ -252,6 +251,18 @@ func parseBundle(r io.ReadSeeker) *Bundle {
 		_, err := r.Seek(int64(4 - header.RegExpStorageSize % 4), io.SeekCurrent)
 		check(err)
 	}
+
+	// Now go back and read regexp table
+	savedOffset, _ = r.Seek(0, io.SeekCurrent)
+	r.Seek(regexpOffset, io.SeekStart)
+	for i := uint32(0); i < header.RegExpCount; i++ {
+		var offset, length uint32
+		check(binary.Read(r, binary.LittleEndian, &offset))
+		check(binary.Read(r, binary.LittleEndian, &length))
+
+		bundle.Regexp = append(bundle.Regexp, regexpStorage[offset:][:length])
+	}
+	r.Seek(savedOffset, io.SeekStart)
 
 	// cjs module table
 	for i := uint32(0); i < header.CjsModuleCount; i++ {
@@ -278,7 +289,7 @@ func main() {
 	fmt.Printf("%d strings\n", len(bundle.Strings))
 	fmt.Printf("%d functions\n", len(bundle.Functions))
 
-	for i := 0; i < len(bundle.Functions); i++ {
+	for i := 239; i < len(bundle.Functions); i++ {
 		// => [Function #0 "global" of 142230 bytes]: 1 params, frame size=21, env size=0, read index sz=15, write index sz=9, strict=0, exc handler=1, debug info=0  @ offset 0x00260a5c
   		// [Exception handlers: [start=0xcf, end=0x11c, target=0x11e] ]
 
@@ -314,6 +325,10 @@ func main() {
 			pc += 4
 			return binary.LittleEndian.Uint32(f.Code[pc - 4:][:4])
 		}
+		f64 := func() float64 {
+			pc += 8
+			return math.Float64frombits(binary.LittleEndian.Uint64(f.Code[pc - 8:][:8]))
+		}
 		for pc < uint32(len(f.Code)) {
 			opOffset := pc
 			fmt.Printf("%06x: ", opOffset)
@@ -321,12 +336,21 @@ func main() {
 
 			switch op {
 			// 0x00: op_unreachable
-			// 0x01: op_new_object_with_buffer
+			case 0x01: // op_new_object_with_buffer
+				v1 := u8()
+				v2 := u16()
+				v3 := u16()
+				v4 := u16()
+				v5 := u16()
+				fmt.Printf("r%d = new_object_with_buffer(size_hint=%d, elements=%d, keys=%d, vals=%d)\n", v1, v2, v3, v4, v5)
 			// 0x02: op_new_object_with_buffer_long
 			case 0x03: // op_new_object(u8_reg)
 				v1 := u8()
 				fmt.Printf("r%d = {}\n", v1)
-			// 0x04: op_new_object_with_parent
+			case 0x04: // new_object_with_parent
+				v1 := u8()
+				v2 := u8()
+				fmt.Printf("r%d = new_object_with_parent(r%d)\n", v1, v2)
 			case 0x05: // op_new_array_with_buffer(reg, u16, u16, u16)
 				v1 := u8()
 				v2 := u16()
@@ -359,7 +383,139 @@ func main() {
 				v1 := u8()
 				v2 := u8()
 				fmt.Printf("r%d = typeof r%d\n", v1, v2)
+			case 0x0e: // op_equality
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d == r%d\n", v1, v2, v3)
+			case 0x0f: // op_strict_equality
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d === r%d\n", v1, v2, v3)
+			case 0x10: // op_inequality
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d != r%d\n", v1, v2, v3)
+			case 0x11: // op_strict_inequality
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d !== r%d\n", v1, v2, v3)
+			case 0x12: // op_less_than
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d < r%d\n", v1, v2, v3)
+			case 0x13: // op_less_than_equals
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d <= r%d\n", v1, v2, v3)
+			case 0x14: // op_greater_than
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d > r%d\n", v1, v2, v3)
+			case 0x15: // op_greater_than_equals
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d >= r%d\n", v1, v2, v3)
+			case 0x16: // op_binary_plus
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d + r%d\n", v1, v2, v3)
+			case 0x17: // op_addition
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d + r%d // numeric\n", v1, v2, v3)
+			case 0x18: // op_multiplication
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d * r%d\n", v1, v2, v3)
+			case 0x19: // op_multiplication_numeric
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d * r%d // numeric\n", v1, v2, v3)
+			case 0x1a: // op_division
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d / r%d\n", v1, v2, v3)
+			case 0x1b: // op_division_numeric
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d / r%d // numeric\n", v1, v2, v3)
+			case 0x1c: // op_remainder
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d % r%d\n", v1, v2, v3)
+			case 0x1d: // op_subtraction
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d + r%d\n", v1, v2, v3)
+			case 0x1e: // op_subtraction_numeric
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d + r%d // numeric\n", v1, v2, v3)
 
+			case 0x1f: // op_shift_left
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d << r%d\n", v1, v2, v3)
+			case 0x20: // op_shift_right_signed
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d >> r%d\n", v1, v2, v3)
+			case 0x21: // op_shift_right_unsigned
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d >>> r%d\n", v1, v2, v3)
+			case 0x22: // op_bitwise_and
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d & r%d\n", v1, v2, v3)
+
+			case 0x24: // op_bitwise_or
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d | r%d\n", v1, v2, v3)
+
+			case 0x25: // op_increment
+				v1 := u8()
+				v2 := u8()
+				fmt.Printf("r%d = r%d + 1\n", v1, v2)
+			case 0x26: // op_decrement
+				v1 := u8()
+				v2 := u8()
+				fmt.Printf("r%d = r%d - 1\n", v1, v2)
+
+			case 0x27: // op_instance_of
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = hermes_instanceof(r%d, r%d)\n", v1, v2, v3)
+
+			case 0x28: // op_in
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d in r%d\n", v1, v2, v3)
 			case 0x29: // op_get_environment(reg, u8)
 				v1 := u8()
 				v2 := u8()
@@ -375,6 +531,10 @@ func main() {
 				v3 := u8()
 				fmt.Printf("environment(r%d, %d) = r%d // non-pointer\n", v1, v2, v3)
 			case 0x2e: // op_load_from_environment(reg, reg, u8)
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("environment(r%d, r%d, %d)\n", v1, v2, v3)
 			case 0x30: // op_get_global_obj(u8_reg)
 				v1 := u8()
 				fmt.Printf("r%d = globalThis\n", v1)
@@ -408,11 +568,60 @@ func main() {
 				v3 := u8()
 				v4 := u16()
 				fmt.Printf("r%d[%q] = r%d // cache=%d\n", v1, bundle.Strings[v4], v2, v3)
+			case 0x3f: // op_put_new_own_by_id(reg, reg, u8)
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d[%q] = r%d // own\n", v1, bundle.Strings[v3], v2)
+			case 0x40: // op_put_new_own_by_id(reg, reg, u16)
+				v1 := u8()
+				v2 := u8()
+				v3 := u16()
+				fmt.Printf("r%d[%q] = r%d // own\n", v1, bundle.Strings[v3], v2)
+			case 0x41: // op_put_new_own_by_id(reg, reg, u32)
+				v1 := u8()
+				v2 := u8()
+				v3 := u32()
+				fmt.Printf("r%d[%q] = r%d // own\n", v1, bundle.Strings[v3], v2)
+			case 0x44: // op_put_own_by_index
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d[%d] = r%d\n", v1, v3, v2)
+			case 0x46: // op_put_own_by_val
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				v4 := u8()
+				fmt.Printf("r%d[r%d] = r%d // enumerable=%d\n", v1, v3, v2, v4)
+			case 0x49: // op_get_by_val(reg, reg, reg)
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = r%d[r%d]\n", v1, v2, v3)
 			case 0x4a: // op_put_by_val(reg, reg, reg)
 				v1 := u8()
 				v2 := u8()
 				v3 := u8()
 				fmt.Printf("r%d[r%d] = r%d\n", v1, v2, v3)
+			case 0x4d: // op_get_prop_name_list(reg, reg, reg, reg)
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				v4 := u8()
+				fmt.Printf("r%d = get_prop_name_list(obj=r%d, it=r%d, size=r%d)\n", v1, v2, v3, v4)
+			case 0x4e: // op_get_next_prop_name(reg, reg, reg, reg, reg)
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				v4 := u8()
+				v5 := u8()
+				fmt.Printf("r%d = get_next_prop_name(list=r%d, obj=r%d, it=r%d, size=r%d)\n", v1, v2, v3, v4, v5)
+			case 0x4f: // op_call
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = call(closure=r%d, %d args)\n", v1, v2, v3)
 			case 0x50: // op_construct(u8_reg, u8_reg, u8)
 				v1 := u8()
 				v2 := u8()
@@ -444,17 +653,30 @@ func main() {
 				v5 := u8()
 				v6 := u8()
 				fmt.Printf("r%d = r%d.bind(r%d)(r%d, r%d, r%d)\n", v1, v2, v3, v4, v5, v6)
+			case 0x59: // op_call_builtin
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = builtins[%d](%d args)\n", v1, v2, v3)
 			case 0x5d: // op_catch(reg)
 				v1 := u8()
 				fmt.Printf("catch(r%d)\n", v1)
 			case 0x5c: // op_ret(reg)
 				v1 := u8()
 				fmt.Printf("return r%d\n", v1)
+			case 0x5f: // op_throw
+				v1 := u8()
+				fmt.Printf("throw r%d\n", v1)
 			case 0x64: // op_create_closure(reg, reg, u16)
 				v1 := u8()
 				v2 := u8()
 				v3 := u16()
 				fmt.Printf("r%d = closure(r%d, %d)\n", v1, v2, v3)
+			case 0x66: // create_generator_closure
+				v1 := u8()
+				v2 := u8()
+				v3 := u16()
+				fmt.Printf("r%d = generator_closure(r%d, %d)\n", v1, v2, v3)
 			case 0x6a: // op_create_this(u8_reg, u8_reg, u8_reg)
 				v1 := u8()
 				v2 := u8()
@@ -481,6 +703,10 @@ func main() {
 				v1 := u8()
 				v2 := u32()
 				fmt.Printf("r%d = const[%d]\n", v1, v2)
+			case 0x70: // load_const_double
+				v1 := u8()
+				v2 := f64()
+				fmt.Printf("r%d = %v\n", v1, v2)
 			case 0x73: // op_load_const_string(u8_reg, u16)
 				v1 := u8()
 				v2 := u16()
@@ -511,31 +737,130 @@ func main() {
 			case 0x7c: // op_load_this_ns(u8_reg)
 				v1 := u8()
 				fmt.Printf("r%d = this\n", v1)
+			case 0x7d: // to_number
+				v1 := u8()
+				v2 := u8()
+				fmt.Printf("r%d = ToNumber(r%d)\n", v1, v2)
+			case 0x7e: // to_numeric
+				v1 := u8()
+				v2 := u8()
+				fmt.Printf("r%d = ToNumeric(r%d)\n", v1, v2)
+			case 0x7f: // to_int32
+				v1 := u8()
+				v2 := u8()
+				fmt.Printf("r%d = r%d | 0\n", v1, v2)
+			case 0x80: // add_empty_string
+				v1 := u8()
+				v2 := u8()
+				fmt.Printf("r%d = \"\" + r%d\n", v1, v2)
+			case 0x81: // op_arguments_prop_by_val
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = arguments[%d] // lazy=r%d\n", v1, v2, v3)
+			case 0x82: // op_arguments_length
+				v1 := u8()
+				v2 := u8()
+				fmt.Printf("r%d = arguments.length // lazy=r%d\n", v1, v2)
+			case 0x83: // op_reify_arguments
+				v1 := u8()
+				fmt.Printf("r%d = arguments\n", v1)
+			case 0x84: // op_create_regexp
+				v1 := u8()
+				v2 := u32()
+				v3 := u32()
+				v4 := u32()
+				fmt.Printf("r%d = regexp(pattern=%q, flags=%q, code=%02x)\n", v1, bundle.Strings[v2], bundle.Strings[v3], bundle.Regexp[v4])
+			case 0x86: // start_generator
+				fmt.Printf("start_generator()\n")
+			case 0x87: // resume_generator
+				v1 := u8()
+				v2 := u8()
+				if v2 == 0 {
+					fmt.Printf("generator_next(r%d)\n", v1)
+				} else {
+					fmt.Printf("generator_return(r%d)\n", v1)
+				}
+			case 0x88: // complete_generator
+				fmt.Printf("complete_generator()\n")
+			case 0x89: // create_generator
+				v1 := u8()
+				v2 := u8()
+				v3 := u16()
+				fmt.Printf("r%d = generator(r%d, %d)\n", v1, v2, v3)
+			case 0x8b: // op_iterator_begin
+				v1 := u8()
+				v2 := u8()
+				fmt.Printf("r%d = iterator(r%d)\n", v1, v2)
+			case 0x8c: // op_iterator_next
+				v1 := u8()
+				v2 := u8()
+				v3 := u8()
+				fmt.Printf("r%d = next(r%d, r%d)\n", v1, v2, v3)
+			case 0x8d: // op_iterator_close
+				v1 := u8()
+				v2 := u8()
+				fmt.Printf("close(r%d, ignore=%d)\n", v1, v2)
 			case 0x8e: // op_jmp(i8)
 				v1 := int8(u8())
 				target := opOffset + uint32(int32(v1))
+				fmt.Printf("goto %06x\n", target)
+			case 0x8f: // op_jmp_long
+				v1 := int32(u32())
+				target := opOffset + uint32(v1)
 				fmt.Printf("goto %06x\n", target)
 			case 0x90: // op_jmp_true(i8, u8_reg)
 				v1 := int8(u8())
 				v2 := u8()
 				target := opOffset + uint32(int32(v1))
 				fmt.Printf("if (r%d) goto %06x\n", v2, target)
+			case 0x91: // op_jmp_true
+				v1 := int32(u32())
+				v2 := u8()
+				target := opOffset + uint32(v1)
+				fmt.Printf("if (r%d) goto %06x\n", v2, target)
 			case 0x92: // op_jmp_false(i8, u8_reg)
 				v1 := int8(u8())
 				v2 := u8()
 				target := opOffset + uint32(int32(v1))
 				fmt.Printf("if (!r%d) goto %06x\n", v2, target)
+			case 0x93: // op_jmp_false_long
+				v1 := u32()
+				v2 := u8()
+				target := opOffset + v1
+				fmt.Printf("if (!r%d) goto %06x\n", v2, target)
+			case 0x94: // op_jmp_undefined(i8, reg)
+				v1 := int8(u8())
+				v2 := u8()
+				target := opOffset + uint32(int32(v1))
+				fmt.Printf("if (r%d === undefined) goto %06x\n", v2, target)
+			case 0x96: // save_generator
+				v1 := int8(u8())
+				target := opOffset + uint32(int32(v1))
+				fmt.Printf("save_generator %06x\n", target)
 			case 0x98: // op_jump_less
 				v1 := int8(u8())
 				v2 := u8()
 				v3 := u8()
 				target := opOffset + uint32(int32(v1))
-				fmt.Printf("if (r%d ? r%d) goto %06x\n", v2, v3, target)
+				fmt.Printf("if (r%d < r%d) goto %06x\n", v2, v3, target)
+			case 0x99: // op_jump_less_long
+				v1 := u32()
+				v2 := u8()
+				v3 := u8()
+				target := opOffset + v1
+				fmt.Printf("if (r%d < r%d) goto %06x\n", v2, v3, target)
 			case 0x9a: // op_jump_not_less
 				v1 := int8(u8())
 				v2 := u8()
 				v3 := u8()
 				target := opOffset + uint32(int32(v1))
+				fmt.Printf("if (r%d ? r%d) goto %06x\n", v2, v3, target)
+			case 0x9b: // op_jump_not_less_long
+				v1 := u32()
+				v2 := u8()
+				v3 := u8()
+				target := opOffset + v1
 				fmt.Printf("if (r%d ? r%d) goto %06x\n", v2, v3, target)
 			case 0x9c: // op_jump_less_number
 				v1 := int8(u8())
@@ -543,11 +868,23 @@ func main() {
 				v3 := u8()
 				target := opOffset + uint32(int32(v1))
 				fmt.Printf("if (r%d ? r%d) goto %06x\n", v2, v3, target)
+			case 0x9d: // op_jump_less_number_long
+				v1 := u32()
+				v2 := u8()
+				v3 := u8()
+				target := opOffset + v1
+				fmt.Printf("if (r%d ? r%d) goto %06x\n", v2, v3, target)
 			case 0x9e: // op_jump_not_less_number
 				v1 := int8(u8())
 				v2 := u8()
 				v3 := u8()
 				target := opOffset + uint32(int32(v1))
+				fmt.Printf("if (r%d ? r%d) goto %06x\n", v2, v3, target)
+			case 0x9f: // op_jump_not_less_number_long
+				v1 := u32()
+				v2 := u8()
+				v3 := u8()
+				target := opOffset + v1
 				fmt.Printf("if (r%d ? r%d) goto %06x\n", v2, v3, target)
 			case 0xa0: // op_jump_less_equal
 				v1 := int8(u8())
@@ -555,17 +892,35 @@ func main() {
 				v3 := u8()
 				target := opOffset + uint32(int32(v1))
 				fmt.Printf("if (r%d ? r%d) goto %06x\n", v2, v3, target)
+			case 0xa1: // op_jump_less_equal_long
+				v1 := u32()
+				v2 := u8()
+				v3 := u8()
+				target := opOffset + v1
+				fmt.Printf("if (r%d ? r%d) goto %06x\n", v2, v3, target)
 			case 0xa2: // op_jump_not_less_equal
 				v1 := int8(u8())
 				v2 := u8()
 				v3 := u8()
 				target := opOffset + uint32(int32(v1))
 				fmt.Printf("if (r%d ? r%d) goto %06x\n", v2, v3, target)
+			case 0xa3: // op_jump_not_less_equal_long
+				v1 := u32()
+				v2 := u8()
+				v3 := u8()
+				target := opOffset + v1
+				fmt.Printf("if (r%d ? r%d) goto %06x\n", v2, v3, target)
 			case 0xa4: // op_jump_less_equal_number
 				v1 := int8(u8())
 				v2 := u8()
 				v3 := u8()
 				target := opOffset + uint32(int32(v1))
+				fmt.Printf("if (r%d ? r%d) goto %06x\n", v2, v3, target)
+			case 0xa5: // op_jump_less_equal_number_long
+				v1 := u32()
+				v2 := u8()
+				v3 := u8()
+				target := opOffset + v1
 				fmt.Printf("if (r%d ? r%d) goto %06x\n", v2, v3, target)
 			case 0xa6: // op_jump_not_less_equal_number
 				v1 := int8(u8())
@@ -578,7 +933,13 @@ func main() {
 				v2 := u8()
 				v3 := u8()
 				target := opOffset + uint32(int32(v1))
-				fmt.Printf("if (r%d ? r%d) goto %06x\n", v2, v3, target)
+				fmt.Printf("if (r%d > r%d) goto %06x\n", v2, v3, target)
+			case 0xa9: // op_jump_greater_long
+				v1 := u32()
+				v2 := u8()
+				v3 := u8()
+				target := opOffset + v1
+				fmt.Printf("if (r%d > r%d) goto %06x\n", v2, v3, target)
 			case 0xaa: // op_jump_not_greater
 				v1 := int8(u8())
 				v2 := u8()
@@ -597,17 +958,35 @@ func main() {
 				v3 := u8()
 				target := opOffset + uint32(int32(v1))
 				fmt.Printf("if (r%d ? r%d) goto %06x\n", v2, v3, target)
+			case 0xaf: // op_jump_not_greater_number_long
+				v1 := u32()
+				v2 := u8()
+				v3 := u8()
+				target := opOffset + v1
+				fmt.Printf("if (r%d ? r%d) goto %06x\n", v2, v3, target)
 			case 0xb0: // op_jump_greater_equal
 				v1 := int8(u8())
 				v2 := u8()
 				v3 := u8()
 				target := opOffset + uint32(int32(v1))
-				fmt.Printf("if (r%d ? r%d) goto %06x\n", v2, v3, target)
+				fmt.Printf("if (r%d >= r%d) goto %06x\n", v2, v3, target)
+			case 0xb1: // op_jump_greater_equal_long
+				v1 := u32()
+				v2 := u8()
+				v3 := u8()
+				target := opOffset + v1
+				fmt.Printf("if (r%d >= r%d) goto %06x\n", v2, v3, target)
 			case 0xb2: // op_jump_not_greater_equal
 				v1 := int8(u8())
 				v2 := u8()
 				v3 := u8()
 				target := opOffset + uint32(int32(v1))
+				fmt.Printf("if (r%d ? r%d) goto %06x\n", v2, v3, target)
+			case 0xb3: // op_jump_not_greater_equal_long
+				v1 := u32()
+				v2 := u8()
+				v3 := u8()
+				target := opOffset + v1
 				fmt.Printf("if (r%d ? r%d) goto %06x\n", v2, v3, target)
 			case 0xb4: // op_jump_greater_equal_number
 				v1 := int8(u8())
@@ -627,11 +1006,23 @@ func main() {
 				v3 := u8()
 				target := opOffset + uint32(int32(v1))
 				fmt.Printf("if (r%d == r%d) goto %06x\n", v2, v3, target)
+			case 0xb9: // op_jump_equal_long
+				v1 := int32(u32())
+				v2 := u8()
+				v3 := u8()
+				target := opOffset + uint32(v1)
+				fmt.Printf("if (r%d == r%d) goto %06x\n", v2, v3, target)
 			case 0xba: // op_jump_not_equal
 				v1 := int8(u8())
 				v2 := u8()
 				v3 := u8()
 				target := opOffset + uint32(int32(v1))
+				fmt.Printf("if (r%d != r%d) goto %06x\n", v2, v3, target)
+			case 0xbb: // op_jump_not_equal_long
+				v1 := int32(u32())
+				v2 := u8()
+				v3 := u8()
+				target := opOffset + uint32(v1)
 				fmt.Printf("if (r%d != r%d) goto %06x\n", v2, v3, target)
 			case 0xbc: // op_jump_strict_equal
 				v1 := int8(u8())
@@ -639,14 +1030,26 @@ func main() {
 				v3 := u8()
 				target := opOffset + uint32(int32(v1))
 				fmt.Printf("if (r%d === r%d) goto %06x\n", v2, v3, target)
+			case 0xbd: // op_jump_strict_equal_long
+				v1 := int32(u32())
+				v2 := u8()
+				v3 := u8()
+				target := opOffset + uint32(v1)
+				fmt.Printf("if (r%d === r%d) goto %06x\n", v2, v3, target)
 			case 0xbe: // op_jump_strict_not_equal
 				v1 := int8(u8())
 				v2 := u8()
 				v3 := u8()
 				target := opOffset + uint32(int32(v1))
 				fmt.Printf("if (r%d !== r%d) goto %06x\n", v2, v3, target)
+			case 0xbf: // op_jump_strict_not_equal_long
+				v1 := int32(u32())
+				v2 := u8()
+				v3 := u8()
+				target := opOffset + uint32(v1)
+				fmt.Printf("if (r%d !== r%d) goto %06x\n", v2, v3, target)
 			default:
-				panic(fmt.Errorf("Unknown bytecode: %02x. Bytes: %02x", op, f.Code[:16]))
+				panic(fmt.Errorf("Unknown bytecode: %02x", op))
 			}
 		}
 	}
